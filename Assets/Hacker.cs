9d3e72cb3253c1197774e6c2c8e94e05647f03f5
Dmitrii <timesburnersteam@gmail.com>
2018-01-16 00:24:29 -0800

    Almost finish

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -7,12 +7,17 @@ public class Hacker : MonoBehaviour {
 	// Use this for initialization
 	// Game state
 	int level;
-	void Start () {
+	enum Screen { MainMenu, Password, Win };
+	Screen currentScreen;
+	string password;
+	void Start () 
+	{
 		ShowMainMenu();
 	}
 	
 	
 	void ShowMainMenu () {
+		currentScreen = Screen.MainMenu;
 		Terminal.ClearScreen();
 		Terminal.WriteLine("What would you like to hack into?");
 		Terminal.WriteLine("Press 1 for the local library");
@@ -22,23 +27,59 @@ public class Hacker : MonoBehaviour {
 		
 		
 	void OnUserInput(string input) {
-		if (input == "menu") {
+		if (input == "menu") 
+		{			// we can always go direct to main menu
 			ShowMainMenu();
-		} else if (input == "007") {
-			Terminal.WriteLine("Please select a level Mr Bond");
-		} else if (input == "1"){
-			level = 1;
-			StartGame(1);
-		} else if (input == "2"){
-			level = 2;
-			StartGame(2); 
-		} else {
-			Terminal.WriteLine("Please choose a valid level");
-		}
-	}
-	void StartGame(int level){
+		} 
+		else if (currentScreen == Screen.MainMenu) 
+		{
+            RunMainMenu(input);
+        }
+		else if (currentScreen == Screen.Password) 
+		{
+            CheckPassword(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        if (input == "007")
+        {
+            Terminal.WriteLine("Please select a level Mr Bond");
+        }
+        else if (input == "1")
+        {
+            level = 1;
+			password = "donkey";
+            StartGame(1);
+        }
+        else if (input == "2")
+        {
+            level = 2;
+			password = "combobulate";
+            StartGame(2);
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+        }
+    }
+
+    void StartGame(int level) 
+	{
+		currentScreen = Screen.Password;
 		Terminal.WriteLine("You have chosen level " + level);
 		Terminal.WriteLine("Please enter your password: ");
 	}
-	
+	void CheckPassword(string input)
+	{
+		if (input == password)
+		{
+			Terminal.WriteLine("WELL DONE!");
+		}
+		else 
+		{
+			Terminal.WriteLine("TRY AGAIN!");
+		}
+	}
 }
